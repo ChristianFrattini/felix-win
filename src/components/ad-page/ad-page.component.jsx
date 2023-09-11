09ed93bbfa05eef8fabc091884ff80162fac4a25
chrifrat1 <chrifrat1@gmail.com>
2023-09-11 20:38:29 +0200

    improvements

--- src/components/ad-page/ad-page.component.jsx
+++ src/components/ad-page/ad-page.component.jsx
@@ -21,8 +21,8 @@ const AdPage=()=>{
                 <button onClick={handleOnClick}>Click</button>
             </div>
             <head>
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7301195125462069"
-                crossorigin="anonymous"></script>
+            {/*<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7301195125462069"
+                crossorigin="anonymous"></script>*/}
             </head>
         </div>
     )
