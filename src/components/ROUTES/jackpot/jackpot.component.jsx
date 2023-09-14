b196d913d16436e2c134e9c34a8b8c51acaf6f91
chrifrat1 <chrifrat1@gmail.com>
2023-09-14 18:47:58 +0200

    update

--- src/components/ROUTES/jackpot/jackpot.component.jsx
+++ src/components/ROUTES/jackpot/jackpot.component.jsx
@@ -1,8 +1,9 @@
 import './jackpot.styles.scss'
-import  {_EXTRACTION_DATE_DAY, _EXTRACTION_DATE_YEAR,_EXTRACTION_DATE_TIME, _EXTRACTION_DATE_MONTH, _JACKPOT, databaseDataFetching}  from '../../../var/var';
+import  {_EXTRACTION_DATE_DAY, _EXTRACTION_DATE_YEAR,_EXTRACTION_DATE_TIME, _EXTRACTION_DATE_MONTH, _JACKPOT, databaseDataFetching, _EXTRACTION_DATE_ISO}  from '../../../var/var';
 import CurrencyConverter from '../../currency-converter/currency-converter.component';
 import { useEffect } from 'react';
 import { useData } from '../../../contexts/data.context';
+import CountdownTimer from '../../countdown-timer/countdown-timer.component';
 
 const Jackpot=()=>{
 
@@ -13,7 +14,8 @@ const Jackpot=()=>{
                 <h2>Jackpot</h2>
                 <h3>The current Jackpot is: </h3>
                 <h2>£{_JACKPOT}</h2>
-                <span>The next lottery draw is set on the {_EXTRACTION_DATE_DAY} {_EXTRACTION_DATE_MONTH} {_EXTRACTION_DATE_YEAR} at {_EXTRACTION_DATE_TIME} GMT </span>
+                <h3>The next lottery draw is set on the {_EXTRACTION_DATE_DAY} {_EXTRACTION_DATE_MONTH} {_EXTRACTION_DATE_YEAR} at {_EXTRACTION_DATE_TIME} GMT </h3>
+                <CountdownTimer targetDate={_EXTRACTION_DATE_ISO}/>
             </div>
             <div>
                 <CurrencyConverter initialAmount={_JACKPOT}/>
